c3ebb29a25d16185f278b487861258ec8d7607f3
AndTolmachev <tolmachevpro@gmail.com>
2019-09-30 00:17:17 +0300

    Урок 1: 1. Задать температуру в градусах по Цельсию. Вывести в alert соответствующую температуру в градусах по Фаренгейту. Подсказка: расчёт идёт по формуле: Tf = (9 / 5) * Tc + 32, где Tf – температура по Фаренгейту, Tc – температура по Цельсию 2. Работа с переменными. a) Объявить две переменные: admin и name. Записать в name строку «Василий»; b) Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var tc = prompt("Введите температуру в градусах Цельсия");
+var tf = tc * 9 / 5 + 32;
+console.log(tc + " градусов по Цельсию равно " + tf + " градусов по Фаренгейту ");
+var name = prompt("Ввидите имя");
+var admin = name;
+console.log("Значение переменной admin равно " + admin);
