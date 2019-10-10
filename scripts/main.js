86e91d28e4b98c2b5ecd7413a828239b78b89e9d
AndTolmachev <tolmachevpro@gmail.com>
2019-10-10 10:05:27 +0300

    оставшиеся задачи сделаю в пятницу

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var s = Math.trunc(Math.random()*10000)
+console.log(s)
+guess(s)
+
+//задание 4)
+for(i=0; i<=9; console.log(i++)){
+    
+}
+//задание 4)*
+var xString = "x"
+for(i=1; i<= 20; i++){
+    console.log(xString)
+    xString += "x"
+}
